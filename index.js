68f0d7ee4f8721c1eed0bda0478c735eb46f2df3
Geraldo Perez <geraldo.perez@twodanz.com>
2017-09-14 17:58:25 -0400

    Added home route

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var express = require('express')
 var app = express()
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 // respond with "hello world" when a GET request is made to the homepage
+// set the home page route
+app.get('/', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('This is the main page of the bot api!');
+});
+
 app.get('/webhook', function (req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
         req.query['hub.verify_token'] === 'my_special_mentor_bot_is_the_token') {
@@ -11,3 +21,7 @@ app.get('/webhook', function (req, res) {
         res.sendStatus(400);
     }
 })
+    
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
