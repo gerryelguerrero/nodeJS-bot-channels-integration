3083ec338e71fb1f2fba0fdf074ce73019a8d967
Geraldo Perez <geraldo.perez@twodanz.com>
2017-09-15 15:15:58 -0400

    Update on body parser implementation

--- index.js
+++ index.js
@@ -3,13 +3,19 @@
  */
 'use strict'
 
-var express = require('express') // NodeJS module - Enables to use express functions
+ /**
+  * NodeJS module - Enables to use express functions
+  */
+var express = require('express')
 var bodyParser = require('body-parser')
-var app = express() // Readability
+
+var app = express() // Initialization for better readability of the code
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
+
+app.use(bodyParser.text({type: 'application/json'}))
 
 // respond with message when a GET request is made to the homepage 
 app.get('/', function(req, res) {
@@ -29,7 +35,15 @@ app.get('/webhook', function (req, res) {
 })
 
 app.post('/webhook', function (req, res) {
-    console.log(bodyParser.json(req.body))
+    try {
+        const data = JSONbig.parse(req.body);
+        console.log(data);   
+    } catch (error) {
+        return res.status(400).json({
+            status: "error",
+            error: err
+        });
+    }
 })
 
 // [TEST] Logs the port and writes to console
