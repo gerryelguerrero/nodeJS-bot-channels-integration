c97aba5260acf295b8138058671a2f6a4be92346
Geraldo Perez <geraldo.perez@twodanz.com>
2017-09-14 20:40:40 -0400

    Added gitignore andwrote docs

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var express = require('express')
-var app = express()
+/**
+ * Copyright (c) 2017 Geraldo A. Perez
+ */
+'use strict'
+
+var express = require('express') // NodeJS module - Enables to use express functions
+var app = express() // Still don't understand this
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// respond with "hello world" when a GET request is made to the homepage
-// set the home page route
+// respond with message when a GET request is made to the homepage 
 app.get('/', function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/plain'}); // Setting content type for head
     res.end('This is the main page of the bot api!');
 });
 
+// respond with validation when a GET request is made to the webhook
 app.get('/webhook', function (req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
         req.query['hub.verify_token'] === 'my_special_mentor_bot_is_the_token') {
@@ -21,7 +26,8 @@ app.get('/webhook', function (req, res) {
         res.sendStatus(400);
     }
 })
-    
+
+// [TEST] Logs the port and writes to console
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
