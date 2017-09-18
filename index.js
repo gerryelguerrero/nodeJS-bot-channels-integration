11f553bfdac5d63b1ca5ff5dcf5c4d8c5cf70c7e
Geraldo Perez <geraldo.perez@twodanz.com>
2017-09-18 13:11:23 -0400

    First test using send api

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
  */
 'use strict'
 
+const FB_PAGE_ACCESS_TOKEN = 'EAAFT9mFPdlYBAATuUVY9rwqAgBi1xJJTSYmRJbOGVyFCZBdzJYVjCIFrvsMCSeUlpVgEy1VOWSZBiSfOg91tgWpfVjSWPw9fJXOswAmziEDiy3q7Ispbu9MuxAeZCK82h8TuDDTyT8dp5jLvTZCxMif6lIPJcUyC3FZBKPUDzE9oHPE3pVMxX'
+
  /**
   * NodeJS module - Enables to use express functions
   */
 var express = require('express')
 var bodyParser = require('body-parser')
+var request = require('request')
 
 var app = express() // Initialization for better readability of the code
 
@@ -34,11 +37,48 @@ app.get('/webhook', function (req, res) {
     }
 })
 
+// respond with the message when a POST request is made to the webhook
 app.post('/webhook', function (req, res) {
     try {
         console.log("We are going to try to get data")
         const data = JSON.parse(req.body);
-        console.log(data);   
+        console.log(data);
+
+        if (data.entry) {
+            let entries = data.entry;
+            entries.forEach((entry) => {
+                let messaging_events = entry.messaging;
+                if (messaging_events) {
+                    messaging_events.forEach((event) => {
+                        if (event.message && !event.message.is_echo) {
+                            return new Promise((resolve, reject) => {
+                                request({
+                                    url: 'https://graph.facebook.com/v2.6/me/messages',
+                                    qs: {access_token: FB_PAGE_ACCESS_TOKEN},
+                                    method: 'POST',
+                                    json: {
+                                        recipient: {id: entry.id},
+                                        message: 'Hey back!'
+                                    }
+                                }, (error, response) => {
+                                    if (error) {
+                                        console.error('Error sending action: ', error);
+                                        reject(error);
+                                    } else if (response.body.error) {
+                                        console.error('Error: ', response.body.error);
+                                        reject(new Error(response.body.error));
+                                    }
+                    
+                                    resolve();
+                                });
+                            });
+                        } else if (event.postback && event.postback.payload) {
+                            console.log('Something different');
+                        }
+                    });
+                }
+            });
+        }
     } catch (err) {
         return res.status(400).json({
             status: "error",
