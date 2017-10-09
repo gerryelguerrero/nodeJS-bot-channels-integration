694acc24e7d10dc07d1e66962aa50d64142239c9
Geraldo Perez <geraldo.perez@twodanz.com>
2017-10-09 16:18:59 -0400

    Changed fb config (2)

--- index.js
+++ index.js
@@ -13,17 +13,16 @@ const request = require('request');
 const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
 const APIAI_LANG = process.env.APIAI_LANG || 'en';
 
+const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN
+const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN
+
 const FacebookBot = require('./facebookbot');
 const FacebookBotConfig = require('./facebookbotconfig');
 
-const botConfig = new FacebookBotConfig(APIAI_ACCESS_TOKEN, APIAI_LANG);
+const botConfig = new FacebookBotConfig(APIAI_ACCESS_TOKEN, APIAI_LANG, FB_PAGE_ACCESS_TOKEN);
 const bot = new FacebookBot(botConfig);
 
 const app = express();// Initialization for better readability of the code
-
-// Constant varibles
-const token = process.env.FB_PAGE_ACCESS_TOKEN
-const verify_token = process.env.FB_VERIFY_TOKEN
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -44,7 +43,7 @@ app.get('/', function(req, res) {
 // respond with validation when a GET request is made to the webhook
 app.get('/webhook', function (req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === verify_token) {
+        req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
             res.send(req.query['hub.challenge']);
     }
     else {
